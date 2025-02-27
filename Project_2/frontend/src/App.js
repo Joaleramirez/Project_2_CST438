dbbf47cff3d78d38fcccfa7a7c924eec370b5079
Joaleramirez <jramirezfausto@csumb.edu>
2025-02-27 14:40:44 -0800

    Fixed App.js

--- Project_2/frontend/src/App.js
+++ Project_2/frontend/src/App.js
@@ -2,15 +2,15 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import TierListPage from "./pages/TierListPage";
 import Dashboard from "./pages/Dashboard";
-
+export default App;
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<WelcomePage />} />
-                <Route path="/TierList" element={<TierListPage />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/" element={<WelcomePage/>}/>
+                <Route path="/TierList" element={<TierListPage/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
             </Routes>
         </Router>
     );
-export default App;
+}
