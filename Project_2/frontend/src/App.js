f889d6b2a708f7481560f0c9c4554562b62c6168
albertogarcia <albergarcia@csumb.edu>
2025-03-05 03:02:39 -0800

    I created the skeleton for the user Onboarding.jsx At the time of this commit, we don't have a database yet. To simulate new user onboarding, I added random status codes to the users to test the functionality. Additionally, I added Explore.jsx to get an idea of how it would be like to view a list of Tier Lists, which the user can then choose from.

--- Project_2/frontend/src/App.js
+++ Project_2/frontend/src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import TierListPage from "./pages/TierListPage";
 import Dashboard from "./pages/Dashboard";
+import Onboarding from "./pages/Onboarding";
+import Explore from "./pages/Explore";
+
 export default App;
 function App() {
     return (
@@ -10,6 +13,8 @@ function App() {
                 <Route path="/" element={<WelcomePage/>}/>
                 <Route path="/TierList" element={<TierListPage/>}/>
                 <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/onboarding" element={<Onboarding />} />
+                <Route path="/explore" element={<Explore />} />
             </Routes>
         </Router>
     );
